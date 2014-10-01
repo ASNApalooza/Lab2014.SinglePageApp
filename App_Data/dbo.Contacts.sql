1585d4260f8d2176c3cf05f268255ac9b767c017
John Nickell <jnickell@asna.com>
2014-10-01 15:46:40 -0500

    local SQL db

---
+++ App_Data/dbo.Contacts.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Contacts] (
+    [id]         INT            IDENTITY (1, 1) NOT NULL,
+    [company]    NVARCHAR (100) NOT NULL,
+    [firstName]  NVARCHAR (100) NOT NULL,
+    [lastName]   NVARCHAR (100) NOT NULL,
+    [email]      NVARCHAR (100) NOT NULL,
+    [phone]      NVARCHAR (100) NOT NULL,
+    [address1]   NVARCHAR (100) NOT NULL,
+    [address2]   NVARCHAR (100) NULL,
+    [city]       NVARCHAR (100) NOT NULL,
+    [state]      NVARCHAR (100) NOT NULL,
+    [postalCode] NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
